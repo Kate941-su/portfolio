2dd1376a643722a3699f95263fb40fcf03cfcec9
海斗 北谷 <valleynorth@outlook.jp>
2023-04-14 16:53:30 +0900

    modelとviewの連携まで完了

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = "portfolio_app"
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('post', views.post_list, name='post'),
+    path('worksTemplate/', views.worksTemplate, name = 'worksTemplate'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -7,4 +7,9 @@ def post_list(request):
     return render(request, 'portfolio_app/post_list.html', {'posts':posts})#データを詰め込んでtemplateに返す
 
 def index(request):
-    return render(request, 'portfolio_app/index.html', {})
+    works = Post.objects.all()
+    return render(request, 'portfolio_app/index.html', {'works' : works})
+
+def worksTemplate(request):
+
+    return render(request, 'portfolio_app/worksTemplate.html', {'works' : works})
