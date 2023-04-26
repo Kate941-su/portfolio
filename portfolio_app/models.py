c7df17ceb9e986f571bf9b1428cd69e4160279f2
海斗 北谷 <valleynorth@outlook.jp>
2023-04-26 15:19:27 +0900

    成果物詳細ページをデザイン

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -9,7 +9,10 @@ class Post(models.Model):
         return self.objects.pk
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='media/', null=True)
+    image1 = models.ImageField(upload_to='media/', null=True)
+    image2 = models.ImageField(upload_to='media/', null=True)
+    image3 = models.ImageField(upload_to='media/', null=True)
+    image4 = models.ImageField(upload_to='media/', null=True)
     urlField = models.CharField(max_length=200,default='')
     aboutField =  models.TextField(default='')
     skillField = models.TextField(default='')
